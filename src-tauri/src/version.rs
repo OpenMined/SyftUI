8e4708768a904036defddd1f24359076f7612775
Tauquir <30658453+itstauq@users.noreply.github.com>
2025-06-01 22:36:25 +0400

    chore: bump versions

--- src-tauri/src/version.rs
+++ src-tauri/src/version.rs
@@ -1,6 +1,6 @@
-pub const DESKTOP_VERSION: &str = "0.1.17";
-pub const DESKTOP_HASH: &str = "e28ac55";
-pub const DESKTOP_BUILD: &str = "2025-06-01T21:10:02+04:00";
+pub const DESKTOP_VERSION: &str = "0.1.18";
+pub const DESKTOP_HASH: &str = "44e6ce7";
+pub const DESKTOP_BUILD: &str = "2025-06-01T22:33:57+04:00";
 pub const DAEMON_VERSION: &str = "0.5.0";
-pub const DAEMON_HASH: &str = "3d20d8b";
-pub const DAEMON_BUILD: &str = "2025-05-29T19:31:19+05:30";
+pub const DAEMON_HASH: &str = "a9bf3c9";
+pub const DAEMON_BUILD: &str = "2025-06-01T20:32:22+05:30";
