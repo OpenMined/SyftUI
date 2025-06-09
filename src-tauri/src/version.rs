35a5c128ebd3222ea391e6d4096a5b7655e98365
Tauquir <30658453+itstauq@users.noreply.github.com>
2025-06-10 03:51:36 +0400

    chore: bump version

--- src-tauri/src/version.rs
+++ src-tauri/src/version.rs
@@ -1,6 +1,6 @@
-pub const DESKTOP_VERSION: &str = "0.1.19";
-pub const DESKTOP_HASH: &str = "a34979e";
-pub const DESKTOP_BUILD: &str = "2025-06-10T03:07:44+04:00";
+pub const DESKTOP_VERSION: &str = "0.1.20";
+pub const DESKTOP_HASH: &str = "1bfd629";
+pub const DESKTOP_BUILD: &str = "2025-06-10T03:50:26+04:00";
 pub const DAEMON_VERSION: &str = "0.5.0";
 pub const DAEMON_HASH: &str = "59e559e";
 pub const DAEMON_BUILD: &str = "2025-06-05T23:22:11+04:00";
