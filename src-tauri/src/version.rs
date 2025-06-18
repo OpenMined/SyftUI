808b5650bef9821fa0154694e7e927e46738982c
Yash Gorana <yash.gorana@hotmail.com>
2025-06-18 20:41:10 +0530

    chore: update version

--- src-tauri/src/version.rs
+++ src-tauri/src/version.rs
@@ -1,6 +1,6 @@
 pub const DESKTOP_VERSION: &str = "0.1.20";
-pub const DESKTOP_HASH: &str = "1bfd629";
-pub const DESKTOP_BUILD: &str = "2025-06-10T03:50:26+04:00";
+pub const DESKTOP_HASH: &str = "864d24a";
+pub const DESKTOP_BUILD: &str = "2025-06-18T20:05:35+05:30";
 pub const DAEMON_VERSION: &str = "0.5.0";
-pub const DAEMON_HASH: &str = "59e559e";
-pub const DAEMON_BUILD: &str = "2025-06-05T23:22:11+04:00";
+pub const DAEMON_HASH: &str = "bbc5a2f";
+pub const DAEMON_BUILD: &str = "2025-06-18T19:58:14+05:30";
